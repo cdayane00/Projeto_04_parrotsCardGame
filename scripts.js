5f01c8c58e018dab71d03a10f5ef9961de7dc069
Carla <carla.dayane.berredo@gmail.com>
2022-04-09 07:51:14 -0400

    distribuição das cartas2

--- scripts.js
+++ scripts.js
@@ -1,24 +1,46 @@
 
 let nCartas = prompt("insira a quantidade de cartas:");
-entrada(nCartas);
+let gifs = [`bobrossparrot.gif`,`explodyparrot.gif`,`fiestaparrot.gif`,`metalparrot.gif`,`revertitparrot.gif`,`tripletsparrot.gif`,`unicornparrot.gif`];
+let aux1=[];
+
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 function entrada(cartas){
     while(cartas > 14 || cartas < 4 || cartas%2!=0){
         cartas = prompt("insira a quantidade de cartas:");
     }
+    embaralha(cartas);
+}
+
+function embaralha(cartas){
     const organizaCartas = document.querySelector(".jogo");
-    for(let i=0; i<cartas;i++){
+    for(let i=0;i<cartas/2;i++){
+        aux1[i]=gifs[i];
+    }
+    for(let i=0;i<cartas/2;i++){
+        aux1.push(gifs[i]);
+    }
+    aux1.sort(comparador);
+    for(let i=0; i<cartas/2;i++){
         organizaCartas.innerHTML += `
         <div class="cartas">
-            <img class="escondido" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/unicornparrot.gif">
-            <img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png">
+            <img class="escondido" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png">
+            <img  src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/${aux1[i]}">
         </div>`;
     }
-    organizaCartas.sort(comparador);
-
+    for(let i=cartas/2;i<cartas;i++){
+        organizaCartas.innerHTML += `
+        <div class="cartas">
+            <img class="escondido" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/front.png">
+            <img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/${aux1[i]}">
+        </div>`;
+    }
 }
 
-function comparador() { 
-	return Math.random() - 0.5; 
-}
+entrada(nCartas);
+
+
 
